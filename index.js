15b52dbd265549abb7672c56735f930ad847f5fd
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-01-04 05:28:38 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    let cards = [];
+    for (let i=0; i<(names.length); i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return cards;
+}
+
+function countDown(number) {
+    let i = number;
+    while (i>=0) {
+        console.log(i--);
+    }
+}
